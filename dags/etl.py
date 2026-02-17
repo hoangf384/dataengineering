83fdaff2db421e6f8d684519129f5e8fedbc4c81
hoangf384 <126351927+hoangf384@users.noreply.github.com>
2026-02-17 08:00:29 +0000

    code: renamed code, added gen data dags

---
+++ dags/etl.py
@@ -0,0 +1,58 @@
+# dags/spark_airflow_connection_client.py
+from datetime import datetime
+from os import getenv
+
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+# env
+AIRFLOW_IP = getenv("AIRFLOW_IP")
+DATABASE_IP = getenv("DATABASE_IP")
+MYSQL_USER = getenv("MYSQL_USER")
+
+MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = getenv("MYSQL_DATABASE")
+CASSANDRA_KEYSPACE = getenv("CASSANDRA_KEYSPACE")
+
+
+PROJECT_ROOT = "/opt/project/code"
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2026, 1, 1),
+    # "email": ["hoangphuc030804@gmail.com"],
+    # "email_on_failure": True,
+}
+
+with DAG(
+    dag_id="ETL-jobs",
+    default_args=default_args,
+    schedule_interval="@hourly",
+    catchup=False,
+) as dag:
+    submit_job = SparkSubmitOperator(
+        task_id="submit_spark_job",
+        conn_id="spark_default",
+        application=f"{PROJECT_ROOT}/extended/main.py",
+        verbose=True,
+        conf={
+            "spark.submit.deployMode": "client",
+            "spark.driver.bindAddress": "0.0.0.0",
+            "spark.driver.host": AIRFLOW_IP,
+            "spark.driver.port": "30000",
+            "spark.blockManager.port": "30001",
+            "spark.driver.memory": "1g",
+            "spark.executor.memory": "1g",
+            "spark.executor.cores": "1",
+        },
+        env_vars={
+            "PYTHONPATH": PROJECT_ROOT,
+            "DATABASE_IP": DATABASE_IP,
+            "MYSQL_USER": MYSQL_USER,
+            "MYSQL_PASSWORD": MYSQL_PASSWORD,
+            "MYSQL_DATABASE": MYSQL_DATABASE,
+            "CASSANDRA_KEYSPACE": CASSANDRA_KEYSPACE,
+        },
+    )
+
+    submit_job
--- dags/gen-data.py
+++ dags/gen-data.py
@@ -1,12 +1,12 @@
-# dags/spark_airflow_connection_client.py
+# dags/gen-data-job-test.py
 from datetime import datetime
 from os import getenv
 
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-AIRFLOW_TAILNET = getenv("AIRFLOW_TAILNET")
 
+AIRLOW_IP = getenv("AIRLOW_IP")
 DATABASE_IP = getenv("DATABASE_IP")
 MYSQL_USER = getenv("MYSQL_USER")
 
@@ -14,33 +14,35 @@ MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = getenv("MYSQL_DATABASE")
 CASSANDRA_KEYSPACE = getenv("CASSANDRA_KEYSPACE")
 
-# 2. Định nghĩa đường dẫn gốc của Code
+
 PROJECT_ROOT = "/opt/project/code"
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2024, 1, 1),
+    "start_date": datetime(2026, 1, 1),
+    # "email": ["hoangphuc030804@gmail.com"],
+    # "email_on_failure": True,
 }
 
 with DAG(
-dag_id="00-ETL-test",
-default_args=default_args,
-schedule_interval=None,
- catchup=False,
+    dag_id="gen-dummy-data",
+    default_args=default_args,
+    schedule_interval="@hourly",
+    catchup=False,
 ) as dag:
     submit_job = SparkSubmitOperator(
-        task_id="submit_spark_job",
+        task_id="submit_dummy_data_generator_job",
         conn_id="spark_default",
-        application=f"{PROJECT_ROOT}/extended/main.py",
+        application=f"{PROJECT_ROOT}/test/dummy-gennerator.py",
         verbose=True,
         conf={
             "spark.submit.deployMode": "client",
             "spark.driver.bindAddress": "0.0.0.0",
-            "spark.driver.host": AIRFLOW_TAILNET,
+            "spark.driver.host": AIRLOW_IP,
             "spark.driver.port": "30000",
             "spark.blockManager.port": "30001",
-            "spark.driver.memory": "512m",
-            "spark.executor.memory": "512m",
+            "spark.driver.memory": "1g",
+            "spark.executor.memory": "1g",
             "spark.executor.cores": "1",
         },
         env_vars={

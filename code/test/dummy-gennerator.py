2ce4f0c2344528a929c9b858d4dc8ca55bc76de6
hoangf384 <126351927+hoangf384@users.noreply.github.com>
2026-02-18 10:49:35 +0000

    code: update insert row

--- code/test/dummy-gennerator.py
+++ code/test/dummy-gennerator.py
@@ -1,13 +1,12 @@
 import logging
+import os
 import random
 import uuid
 from datetime import datetime, timezone
 from typing import Tuple
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-
-import os
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 
 # environment variables
 DATABASE_IP = os.getenv("DATABASE_IP", "localhost")
@@ -16,7 +15,7 @@ MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "123456")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "data_engineering")
 MYSQL_URL = f"jdbc:mysql://{DATABASE_IP}:3306/{MYSQL_DATABASE}?allowPublicKeyRetrieval=true&useSSL=false"
 CASSANDRA_KEYSPACE = os.getenv("CASSANDRA_KEYSPACE", "recruitment")
-NUM_100NS_INTERVALS_SINCE_UUID_EPOCH = 0x01b21dd213814000
+NUM_100NS_INTERVALS_SINCE_UUID_EPOCH = 0x01B21DD213814000
 
 # logging setup
 logging.basicConfig(
@@ -55,30 +54,43 @@ def get_reference_data(spark: SparkSession) -> Tuple[list, list]:
     logger.info("Loading reference data from MySQL via Spark...")
 
     # 1.
-    jobs_df = spark.read.format("jdbc") \
-        .option("url", MYSQL_URL) \
-        .option("dbtable", "(SELECT id as job_id, campaign_id, group_id FROM job) as tmp") \
-        .option("driver", "com.mysql.cj.jdbc.Driver") \
-        .option("user", MYSQL_USER) \
-        .option("password", MYSQL_PASSWORD).load()
+    jobs_df = (
+        spark.read.format("jdbc")
+        .option("url", MYSQL_URL)
+        .option(
+            "dbtable", "(SELECT id as job_id, campaign_id, group_id FROM job) as tmp"
+        )
+        .option("driver", "com.mysql.cj.jdbc.Driver")
+        .option("user", MYSQL_USER)
+        .option("password", MYSQL_PASSWORD)
+        .load()
+    )
 
     jobs_list = jobs_df.collect()
 
     # 2. Đọc Publisher
-    pub_df = spark.read.format("jdbc") \
-        .option("url", MYSQL_URL) \
-        .option("dbtable", "(SELECT distinct(id) as publisher_id FROM master_publisher) as tmp") \
-        .option("driver", "com.mysql.cj.jdbc.Driver") \
-        .option("user", MYSQL_USER) \
-        .option("password", MYSQL_PASSWORD).load()
+    pub_df = (
+        spark.read.format("jdbc")
+        .option("url", MYSQL_URL)
+        .option(
+            "dbtable",
+            "(SELECT distinct(id) as publisher_id FROM master_publisher) as tmp",
+        )
+        .option("driver", "com.mysql.cj.jdbc.Driver")
+        .option("user", MYSQL_USER)
+        .option("password", MYSQL_PASSWORD)
+        .load()
+    )
 
-    pub_list = [row['publisher_id'] for row in pub_df.collect()]
+    pub_list = [row["publisher_id"] for row in pub_df.collect()]
 
     print(f"Loaded {len(jobs_list)} jobs and {len(pub_list)} publishers.")
     return jobs_list, pub_list
 
 
-def generate_and_write_batch(spark: SparkSession, n_records: int, jobs_list: list, pub_list: list):
+def generate_and_write_batch(
+    spark: SparkSession, n_records: int, jobs_list: list, pub_list: list
+):
     """
     Generate fake data and write to Cassandra
     """
@@ -89,9 +101,11 @@ def generate_and_write_batch(spark: SparkSession, n_records: int, jobs_list: lis
         job = random.choice(jobs_list)
         pub_id = random.choice(pub_list)
 
-
         bid = random.randint(0, 1)
-        custom_track = random.choices(['click', 'conversion', 'qualified', 'unqualified'], weights=(70, 10, 10, 10))[0]
+        custom_track = random.choices(
+            ["click", "conversion", "qualified", "unqualified"],
+            weights=(70, 10, 10, 10),
+        )[0]
 
         # --- SỬA 1: Định nghĩa biến 'now' trước ---
         u = uuid.uuid1()
@@ -104,44 +118,48 @@ def generate_and_write_batch(spark: SparkSession, n_records: int, jobs_list: lis
         event_date = dt_object.strftime("%Y-%m-%d")
         # -----------------------------------------
 
-        group_id = int(job['group_id']) if job['group_id'] else 0
+        group_id = int(job["group_id"]) if job["group_id"] else 0
 
-        data.append((
-            event_date,
-            create_time,
-            bid,
-            int(job['campaign_id']),
-            custom_track,
-            group_id,
-            int(job['job_id']),
-            pub_id,
-            ts,
-        ))
+        data.append(
+            (
+                event_date,
+                create_time,
+                bid,
+                int(job["campaign_id"]),
+                custom_track,
+                group_id,
+                int(job["job_id"]),
+                pub_id,
+                ts,
+            )
+        )
 
     # 2. create Spark DataFrame
-    schema = StructType([
-        StructField("event_date", StringType(), False),
-        StructField("create_time", StringType(), False),
-        StructField("bid", IntegerType(), True),
-        StructField("campaign_id", IntegerType(), True),
-        StructField("custom_track", StringType(), True),
-        StructField("group_id", IntegerType(), True),
-        StructField("job_id", IntegerType(), True),
-        StructField("publisher_id", IntegerType(), True),
-        StructField("ts", StringType(), True)
-    ])
+    schema = StructType(
+        [
+            StructField("event_date", StringType(), False),
+            StructField("create_time", StringType(), False),
+            StructField("bid", IntegerType(), True),
+            StructField("campaign_id", IntegerType(), True),
+            StructField("custom_track", StringType(), True),
+            StructField("group_id", IntegerType(), True),
+            StructField("job_id", IntegerType(), True),
+            StructField("publisher_id", IntegerType(), True),
+            StructField("ts", StringType(), True),
+        ]
+    )
 
     batch_df = spark.createDataFrame(data, schema)
 
     # 3. write into Cassandra
     logging.info(f"Writing {n_records} records to Cassandra...")
     (
-        batch_df.write
-        .format("org.apache.spark.sql.cassandra")
+        batch_df.write.format("org.apache.spark.sql.cassandra")
         .options(keyspace=CASSANDRA_KEYSPACE, table="tracking")
         .mode("append")
         .save()
     )
+
 
 if __name__ == "__main__":
     spark = get_spark_session()
@@ -154,7 +172,7 @@ if __name__ == "__main__":
 
     logger.info("STARTING GENERATOR")
 
-    n = random.randint(10, 100)
+    n = random.randint(10000, 30000)
     generate_and_write_batch(spark, n, jobs_data, pubs_data)
 
     logger.info("GENERATION FINISHED")
